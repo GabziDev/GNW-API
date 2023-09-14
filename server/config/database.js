a99738783b4b2fc23eba9581a7faab69347d6f49
minecraft2.ggabz190516@gmail.com <minecraft2.ggabz190516@gmail.com>
2023-09-14 20:59:48 +0200

    v1

--- server/config/database.js
+++ server/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectionMongo = async () => {
     try {
         mongoose.set('strictQuery', false);
-        mongoose.connect('mongodb://admin:ybv76GQg53Xi2C@163.5.143.222:30001/gnw-api?authSource=admin&readPreference=primary&ssl=false');
+        mongoose.connect('mongodb://');
     } catch {
 
     }
