bcbf54041217116b33f87ba769e5ca1831a58e39
minecraft2.ggabz190516@gmail.com <minecraft2.ggabz190516@gmail.com>
2023-09-14 20:30:05 +0200

    v1

--- server/server.js
+++ server/server.js
@@ -1,2 +1,14 @@
 //GNW-API
 //v1
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get("/test", (req, res) => {
+    res.json({ message: "Ok !" });
+});
+
+app.listen(port, () => {
+    console.log("Serveur démarré !");
+});
