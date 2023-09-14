86eb227ef4b8e9b4d670056c60d56be83ff49f8c
minecraft2.ggabz190516@gmail.com <minecraft2.ggabz190516@gmail.com>
2023-09-14 21:33:48 +0200

    add database

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 //GNW-API
 //v1.0
 const express = require('express');
+const dotenv = require('dotenv').config();
+const connectionMongo = require('./config/database');
 
 const app = express();
 const port = 3000;
+
+// Connexion à la base de données
+connectionMongo();
 
 // Traiter les données de la requete
 app.use(express.json());
